f0b93b238b439e37ff88160f22378fd0601b9db5
JCampos44 <jecv.dev@gmail.com>
2026-03-27 13:16:41 -0300

    feat: integrate Dedoc Scramble for API documentation generation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Dedoc\Scramble\Scramble;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Scramble::configure()
+            ->expose(
+                ui: '/docs/v1/api',
+                document: '/docs/v1/api.json'
+            );
+
+        Gate::define('viewApiDocs', function ($user = null) {
+            return true;
+        });
     }
 }
